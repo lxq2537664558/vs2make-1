66c18a87e4f691f9ddd512e918a51d964f6dc119
vasily-knk <vasily.knk@gmail.com>
2015-01-21 21:47:10 +0300

    ItemDefinitionGroup

--- vs2make.py
+++ vs2make.py
@@ -1,12 +1,16 @@
 import xml.etree.ElementTree as ET
 from collections import namedtuple
 
-Project = namedtuple('Project', ['sources', 'headers'])
+
+class Project:
+    sources = []
+    headers = []
+    customs = []
+
 
 
 class Parser:
-    project = Project([], [])
-    include_f = None
+    project = Project()
 
     def check_condition(self, e):
         if 'Condition' in e.attrib:
@@ -16,38 +20,52 @@ class Parser:
         else:
             return True
 
-    def mk(self, root, parsers):
+    def mk(self, root, parsers, skip_unknown=True):
         for child in root:
             if child.tag in parsers:
                 if self.check_condition(child):
                     parsers[child.tag](child)
+            else:
+                if not skip_unknown:
+                    message = 'Unknown tag: ' + child.tag
+                    raise Exception(message)
 
-    def parse_file(self, e, include_f):
-        # Ugly ugly ugly hack: don't know how to assign from inside of lambda
-        exclude = []
-        parsers = {
-            'ExcludedFromBuild': lambda child: exclude.append('Fuck')
-        }
-        self.mk(e, parsers)
-        if not exclude:
-            include_f(e.attrib['Include'])
+    def parse_project_configuration(self, e):
+        pass
+
+    def parse_custom_build(self, e):
+        self.project.customs.append(e.attrib['Include'])
+
+    def parse_source(self, e):
+        self.project.sources.append(e.attrib['Include'])
+
+    def parse_header(self, e):
+        self.project.headers.append(e.attrib['Include'])
 
     def parse_item_group(self, e):
         parsers = {
-            'ClCompile': lambda child: self.parse_file(child, lambda name: self.project.sources.append(name)),
-            'ClInclude': lambda child: self.parse_file(child, lambda name: self.project.headers.append(name)),
+            'ClCompile': self.parse_source,
+            'ClInclude': self.parse_header,
+            'CustomBuild': self.parse_custom_build,
+            'ProjectConfiguration': self.parse_project_configuration,
         }
-        self.mk(e, parsers)
+        self.mk(e, parsers, skip_unknown=False)
+
+    # ItemDefinitionGroup
+    def parse_item_definition_group(self, e):
+        pass
+
 
     def parse_project(self, e):
         parsers = {
-            'ItemGroup': self.parse_item_group
+            'ItemGroup': self.parse_item_group,
+            'ItemDefinitionGroup': self.parse_item_definition_group,
         }
         self.mk(e, parsers)
 
     def parse(self, tree):
         root = tree.getroot()
-        self.project = Project([], [])
+        self.project = Project()
         self.parse_project(root)
         return self.project
 
@@ -57,8 +75,9 @@ def parse_tree(tree):
     return parser.parse(tree)
 
 
-tree = ET.parse('test_proj.vcxproj.xml')
+tree = ET.parse('objects.vcxproj.xml')
 project = parse_tree(tree)
 
 print project.sources
 print project.headers
+print project.customs
